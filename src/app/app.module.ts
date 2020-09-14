ea62dc9b58f12da8fcaddeaf44af83da162cd8ce
Tfin2001 <45980335+Tfin2001@users.noreply.github.com>
2020-09-14 17:24:26 -0400

    adding files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HoursComponent } from './hours/hours.component';
+import { ServicesComponent } from './services/services.component';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HoursComponent,
+    ServicesComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
